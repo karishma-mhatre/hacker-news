ae8a358b775fcf5a0aa1bc62db3e14edb3590583
karishma-mhatre <karishmamhatre077@gmail.com>
2020-07-31 22:53:44 +0530

    Updates

---
+++ precache-manifest.67da4ae749a9234a21420079ee46eec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43625b60ce45a6a4853e1fb06cb2b8f",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "686f895fc375703fc3ff",
+    "url": "/hacker-news/static/css/main.99ca87b3.chunk.css"
+  },
+  {
+    "revision": "8c782319999269a35145",
+    "url": "/hacker-news/static/js/2.01971658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news/static/js/2.01971658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686f895fc375703fc3ff",
+    "url": "/hacker-news/static/js/main.033a8859.chunk.js"
+  },
+  {
+    "revision": "3058bd9c9b16bc5cbe6d",
+    "url": "/hacker-news/static/js/runtime-main.b66f509d.js"
+  }
+]);
